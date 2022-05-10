c3930400469d1a3224efdd03236e28b1f2e14eb0
Marianasantv <103968769+Marianasantv@users.noreply.github.com>
2022-05-10 09:47:49 -0600

    Create Largestnumber.py

---
+++ Listas.py/Largestnumber.py
@@ -0,0 +1,7 @@
+
+def largest_number(arr):
+    Max = list[0]
+    for i in range (len(list)):
+        if (list[i]> Max):
+        Max = list[i]
+print("El valor máximo de la lista es: " +str(Max)) 
