2b14cbbe359b028d0067347f47f201fb569ebbf6
Marianasantv <103968769+Marianasantv@users.noreply.github.com>
2022-05-10 09:46:39 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+list=[5,10,3,7,4,9,8]
+
+
+
+
+### BUBBLE SORT 
+
+
+print("   ")
+print ("Bubble sort: ")
+print("   ")
+
+# from Sorting import bubblesort 
+# bubblesort.bubble_sort(list)
+
+print("   ")
+
+
+#### LISTAS 
+print("   ")
+print ("Largest number in list: ")
+print("   ")
+
+from Listas.py import largestnumber 
+largestnumber.largest_number(list)
+
+
+
+
+
+
+
+
+
+
+
