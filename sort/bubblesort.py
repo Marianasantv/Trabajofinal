efdfa62d46a1bf887e9c4f6901b810649ba4963d
Marianasantv <103968769+Marianasantv@users.noreply.github.com>
2022-05-10 22:58:24 -0600

    Update bubblesort.py

--- sort/bubblesort.py
+++ sort/bubblesort.py
@@ -1,10 +1,18 @@
 def bubble_sort(arr):
 
-    for i in range(len(arr)):
-        for j in range(len(arr) - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr)-1): #termino hasta el penultimo indice para comparar con el de la derecha
+            if arr[j] > arr[j+1]:
+                #swap python: a, b = b, a
+                arr[j], arr[j+1] = arr[j+1] , arr[j]
     
     return arr
-list=[5,10,3,7,4,9,8]
-print(bubble_sort(list))
+'''
+unsorted_list = [-2,6,4,67,13]
+print("Lista deordenada: " , unsorted_list)
+
+sorted_list = bubble_sort(unsorted_list)
+print("Lista ordenada: " , sorted_list)
+'''
+
